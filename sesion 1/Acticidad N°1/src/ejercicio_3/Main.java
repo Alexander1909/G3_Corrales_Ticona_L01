7bef4a605380d7a7e21728543078532bac8fcf0d
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-20 20:42:37 -0500

    v8

---
+++ sesion 1/Acticidad N°1/src/ejercicio_3/Main.java
@@ -0,0 +1,11 @@
+package ejercicio_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p1 = new Persona();
+        p1.asignarNombre("Lucia");
+        p1.mostrarNombre();
+
+    }
+
+}
---
+++ sesion 1/Acticidad N°1/src/ejercicio_3/Persona.java
@@ -0,0 +1,16 @@
+package ejercicio_3;
+
+public class Persona {
+    private String nombre;
+
+    public void setNombre(){
+        this.nombre=nombre;
+
+    }
+
+    public void mostrarNombre(){
+        System.out.println("Mi nombre es" + this.nombre);
+
+    }
+
+}
