ab8936b44d239b9f455608a6ccf6ab7eb2c9d708
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-20 20:52:19 -0500

    v9

--- sesion 1/Acticidad N°1/src/ejercicio_4/Main.java
+++ sesion 1/Acticidad N°1/src/ejercicio_4/Main.java
@@ -1,5 +1,14 @@
 package ejercicio_4;
 
 public class Main {
+    public static void main(String[] args) {
+        Estudiante [] grupo = new Estudiante[3];
+
+        grupo[0]= new Estudiante("ana ", 20);
+        grupo[1]= new Estudiante("pedro", 22);
+        grupo[2]= new Estudiante("luis", 19);
+
+
+    }
 
 }
