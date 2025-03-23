c91acef119dbbd79cc69bdcd7e6b2d1c39724bb2
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-23 11:20:15 -0500

    v13

--- sesion 1/Acticidad N°1/src/Actividad_N1/Rectangulo.java
+++ sesion 1/Acticidad N°1/src/Actividad_N1/Rectangulo.java
@@ -1,3 +1,23 @@
-public class Rectangulo {
+import java.util.*;
 
+public class Rectangulo {
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+
+    public Rectangulo(Coordenada esquina1, Coordenada esquina2){
+        this.esquina1=esquina1;
+        this.esquina2=esquina2;
+    }
+
+    public Coordenada getEsquina1(){
+        return esquina1;
+    }
+
+    public Coordenada getEsquina2(){
+        return esquina2;
+    }
+
+    public double calculoArea(){
+        double base =
+    }
 }
