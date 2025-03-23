95588e619e1844492833ad7cc9ad7db1a8d5a6b9
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-23 13:25:21 -0500

    v22

--- sesion 1/Acticidad N°1/src/Actividad_N1/Main.java
+++ sesion 1/Acticidad N°1/src/Actividad_N1/Main.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 public class Main {
-    public static Rectangulo Rectangulotest(Rectangulo r1, Rectangulo r2){
-        double minimo=Math.max(, b)
-    }
-        
-    }
-}
+    public class Principal {
+    public static Rectangulo rectanguloSobre(Rectangulo r1, Rectangulo r2) {
+        double minX = Math.max(Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX()),
+        Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
+        double maxX = Math.min(Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX()),
+        Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
+
+        double minY = Math.max(Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY()),
+        Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
+        double maxY = Math.min(Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY()),
+        Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
+
