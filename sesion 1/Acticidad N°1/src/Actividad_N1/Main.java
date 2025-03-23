eda9269b43c96bec64be8f9f92976298b3b9258c
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-23 13:18:06 -0500

    v21

---
+++ sesion 1/Acticidad N°1/src/Actividad_N1/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static Rectangulo Rectangulotest(Rectangulo r1, Rectangulo r2){
+        double minimo=Math.max(, b)
+    }
+        
+    }
+}
