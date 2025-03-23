74c3517c4f5bf59b75ec69c55acf48088a37613f
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-23 11:03:12 -0500

    v10

---
+++ sesion 1/Acticidad N°1/src/Actividad_N1/Coordenada.java
@@ -0,0 +1,14 @@
+public class Coordenada {
+    private double x;
+    private double Y;
+
+    public Coordenada(){
+        this.x=0;
+        this.y=0;
+
+    public Coordenada( double x, double y){
+        
+    }
+
+    }
+}
