6e093e4f4227b842af0a17db5c14c3c1d88230d5
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-23 15:30:04 -0500

    v23

--- sesion 1/Acticidad N°1/src/Actividad_N1/Main.java
+++ sesion 1/Acticidad N°1/src/Actividad_N1/Main.java
@@ -3,12 +3,30 @@ public class Main {
     public class Principal {
     public static Rectangulo rectanguloSobre(Rectangulo r1, Rectangulo r2) {
         double minX = Math.max(Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX()),
-        Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
+            Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
         double maxX = Math.min(Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX()),
-        Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
+            Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX()));
 
         double minY = Math.max(Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY()),
-        Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
+            Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
         double maxY = Math.min(Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY()),
-        Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
+            Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY()));
+
+        return (minX < maxX && minY < maxY) ? new Rectangulo(new Coordenada(minX, minY), new Coordenada(maxX, maxY)) : null;
+
+    }
+
+    public static void mostrarRectangulo(Rectangulo r){
+        System.out.println(r);
+    }
+
+    private static Coordenada leerCoordenada(Scanner sc, String mensaje){
+        Scanner sc = new Scanner(System.in);
+
+        Coordenada e1= leerCoordenada(sc,"Ingrese una esquina del 1er rectangulo:");
+        Coordenada e2= leerCoordenada(sc, "Ingrese la esquina opuesta del 1er rectángulo:")
+    }
+
+
+}
 
